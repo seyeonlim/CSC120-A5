35a37b3ea581bc0286657209c1ada84059c93aa1
Seyeon Lim <slim@smith.edu>
2023-03-03 19:26:17 -0500

    changed constructor

--- Engine.java
+++ Engine.java
@@ -1,37 +1,12 @@
 public class Engine {
 
-    private String FuelType;
-    double currentFuelLv;
-    double maxFuelLv;
+    private FuelType fuelType;
+    private double currentFuelLv;
+    private double maxFuelLv;
 
-    public Engine(String FuelType, double currentFuelLv, double maxFuelLv) {
-        this.FuelType = FuelType;
+    public Engine(FuelType fuelType, double currentFuelLv, double maxFuelLv) {
+        this.fuelType = fuelType;
         this.currentFuelLv = currentFuelLv;
-        this.maxFuelLv = maxFuelLv;
-
-    }
-    
-    public String getFuelType() {
-        return FuelType;
-    }
-
-    public double getCurrentFuelLv() {
-        return currentFuelLv;
-    }
-
-    public double getMaxFuelLv() {
-        return maxFuelLv;
-    }
-    
-    public void setFuelType(String FuelType) {
-        this.FuelType = FuelType;
-    }
-
-    public void setCurrentFuelLv(double currentFuelLv) {
-        this.currentFuelLv = currentFuelLv;
-    }
-
-    public void setMaxFuelLv(double maxFuelLv) {
         this.maxFuelLv = maxFuelLv;
     }
 
@@ -43,7 +18,37 @@ public class Engine {
         if (this.currentFuelLv <= 0) {
             throw new RuntimeException("Train is out of fuel. Please refuel.");
         }
-        this.currentFuelLv -= 0.5;
+        this.currentFuelLv -= 10;
         System.out.println("Remaining fuel level: " + this.currentFuelLv);
     }
+
+    public static void main(String[] args) {
+        Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0, 200.0);
+        try {
+            while (true) {
+                myEngine.go();
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage()); // Out of fuel
+        }
+
+        myEngine.refuel();
+        
+        try {
+            while (true) {
+                myEngine.go();
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage()); // Out of fuel
+        }
+    }
 }
+/*We recommend you start by implementing the `Engine` class. Your `Engine` class will need:
+ 
+  - a private `FuelType` attribute to indicate what type of fuel it uses, and `double`s to store the 
+  current and maximum fuel levels (along with an approproate accessors for each)
+  - a constructor, which takes in initial values for the attributes named above and sets them appropriately
+  - a method `public void refuel()` which will reset the `Engine`'s current fuel level to the maximum
+  - a method `public void go()` which will decrease the current fuel level and print some useful information 
+  (e.g. remaining fuel level) provided the fuel level is above 0 (otherwise it should throw a `RuntimeException` containing an informative message)
+*/
